efbe1a70474d5b0283aebda41d1c70db7af633a6
koeppwesley2002 <koeppwesley@gmail.com>
2025-12-01 21:50:01 -0300

    colocado arquivo theme na pasta

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
 import { Groups } from "@screens/groups";
+import { ThemeProvider } from "styled-components/native";
+import theme from './src/theme';
 
 export default function App() {
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      <Groups />
+    </ThemeProvider>
   );
 }
