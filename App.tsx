a7c6265a5bdc2b9ae5cf9604b1d0fef424b4ee8f
koeppwesley2002 <koeppwesley@gmail.com>
2025-12-04 23:45:23 -0300

    criando componentes

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
-import { Groups } from "@screens/groups";
+import { Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import { ThemeProvider } from "styled-components/native";
-import theme from './src/theme';
+
+import { Groups } from "@screens/groups";
+
+import theme from "src/theme";
+import { ActivityIndicator } from "react-native";
+import { Loading } from "@components/Loading";
 
 export default function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <Groups />
-    </ThemeProvider>
-  );
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
+   return (
+     <ThemeProvider theme={theme}>
+        { !fontsLoaded ? <Groups /> : <Loading /> }
+     </ThemeProvider>
+   );
 }
--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -1,10 +1,9 @@
-import { StyleSheet, Text, View} from 'react-native';
 import { Container, Title } from './styles';
 
 export function Groups() {
-    return (
-        <Container>
-            <Title>Hello world</Title>
-        </Container>
-    )
+  return (
+     <Container>
+        <Title>Groups!</Title>
+     </Container>
+   )
 }
